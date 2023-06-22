81c76c8d565650b7e1edb78d641fd6f32e067f35
Annee Mahida <Annee Mahida@LAPTOP-L5E45TAD>
2023-06-22 14:00:05 -0400

    Add project files.

---
+++ Question3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ProblemSet
+{
+    class Question3
+    {
+        static void Main(string[] args)
+        {
+            
+            double s, a, b, c, AreaOfTriangle;
+
+            
+            Console.WriteLine("Enter the values of a, b, c: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());
+            c = Convert.ToDouble(Console.ReadLine());
+
+            
+            s = (a + b + c) / 2;
+
+           
+            AreaOfTriangle = Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+
+            
+            Console.WriteLine("Area of triangle is " + AreaOfTriangle);
+            Console.ReadLine();
+        }
+    }
+}
